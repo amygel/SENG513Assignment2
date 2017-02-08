d06f97e8c812f0182a7d3635a0df7d8a46219292
Amy Gelowitz <amygelowitz@Jamess-MacBook-Pro.local>
2017-02-08 12:27:13 -0700

    Calculating number of words

--- code.js
+++ code.js
@@ -3,10 +3,11 @@
 //
 function getStats(txt) {
     let nChars = getNumberOfChars(txt)
+    let nWords = getNumberOfWords(txt)
 
     return {
         nChars: nChars,
-        nWords: 22,
+        nWords: nWords,
         nLines: 10,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
@@ -19,4 +20,16 @@ function getStats(txt) {
 
 function getNumberOfChars(txt) {
     return txt.length
+}
+
+function getNumberOfWords(txt) {
+    let count = 0
+    var arr;
+
+    var re = /([-A-Za-z0-9])+/g;
+
+    while ((arr = re.exec(txt)) !== null) {
+        count++;
+    }
+    return count
 }
